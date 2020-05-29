def8622a0289cdd1553980691018c9f49b8ddb6f
Michael Vo <zax@zaxu.xyz>
2020-05-29 20:13:57 +1000

    remove aspnet comments

--- js/site.js
+++ js/site.js
@@ -1,7 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿/ Write your JavaScript code.
 var roxCounter = 0;
 var twilightCounter = 0;
 var hollowCounter = 0;
