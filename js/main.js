5951cd66a8228dad13a9ce328bf64a403e331750
Michael Vo <zax@zaxu.xyz>
2020-06-03 23:04:00 +1000

    reorganise script

--- js/main.js
+++ js/main.js
@@ -2,30 +2,25 @@
 document.querySelectorAll(".grid > div").forEach(element => {
   element.onclick = (event) => {
     const elem = event.currentTarget;
-
-    let total = elem.dataset.total || 1;
     const icon = elem.querySelector(".icon");
+    const nobody = elem.querySelector(".nobody");
 
     // Level is the number associated - like 2nd/3rd visits or lvl 2 drive
     let level = Number(elem.dataset.level) || 0;
-
-    const nobody = elem.querySelector(".nobody");
-
-    if (nobody) {
-      total = Number(total) + 2;
-    } else {
-      total = Number(total) + 1;
-    }
+    let total = elem.dataset.total || 1;
 
     // Increase level, resetting to 0 if it reaches the max
+    total = Number(total) + (nobody ? 2 : 1);
     level = (level + 1) % (total);
     elem.dataset.level = level;
 
+    // Add nobody symbol on the last level
     const addNobody = nobody && level === total - 1;
 
     // Set the number element source to the corresponding image
     const number = elem.querySelector(".number");
     if (number && level > 0 && !addNobody) {
+      // Update number if this isn't the nobody level
       number.setAttribute("src", `img/numbers/${level}.png`);
     }
 
