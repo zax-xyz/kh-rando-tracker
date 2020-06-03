670cf17bd3bf42fdd876606bc4e1ca702753dcac
Michael Vo <zax@zaxu.xyz>
2020-06-03 22:46:24 +1000

    fix onclick targeting

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 ﻿// Icon clicking
 document.querySelectorAll(".grid > div").forEach(element => {
   element.onclick = (event) => {
-    const elem = event.target.parentElement;
+    const elem = event.target;
 
     let total = elem.dataset.total || 1;
     const icon = elem.querySelector(".icon");
