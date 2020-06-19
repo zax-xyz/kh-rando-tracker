50ba07504bb7a919096d3db83e20d0bf512c2291
Michael Vo <zax@zaxu.xyz>
2020-06-19 13:58:01 +1000

    use one generalised items object

--- items.js
+++ items.js
@@ -1,39 +1,50 @@
 {
-  worlds: {
-    "Roxas World": {},
-    "Twilight Town": { total: 3, },
-    "Hollow Bastion": { total: 3, secondary: "fenrir", },
-    "Land of Dragons": { total: 2, },
-    "Olympus Coliseum": { total: 2, },
-    "Beast's Castle": { total: 2, },
-    "Port Royal": { total: 2, },
-    "Agrabah": { total: 2, },
-    "Halloween Town": { total: 2, },
-    "Pride Lands": { total: 2, },
-    "Disney Castle": { secondary: "terra", },
-    "Space Paranoids": { total: 2, noData: true, },
-    "The World That Never Was": {},
-    "Atlantica": { total: 5, },
-    "100 Acre Wood": { total: 6, noData: true, },
-  },
+  items: {
+    // Worlds
+    "Roxas World": { file: "worlds/roxas world", data: true },
+    "Twilight Town": { file: "worlds/twilight town", total: 3, data: true, },
+    "Hollow Bastion": { file: "worlds/hollow bastion", total: 3, data: true, secondary: "fenrir", },
+    "Land of Dragons": { file: "worlds/land of dragons", total: 2, data: true, },
+    "Olympus Coliseum": { file: "worlds/olympus coliseum", total: 2, data: true, },
+    "Beast's Castle": { file: "worlds/beast's castle", total: 2, data: true, },
+    "Port Royal": { file: "worlds/port royal", total: 2, data: true, },
+    "Agrabah": { file: "worlds/agrabah", total: 2, data: true, },
+    "Halloween Town": { file: "worlds/halloween town", total: 2, data: true, },
+    "Pride Lands": { file: "worlds/pride lands", total: 2, data: true, },
+    "Disney Castle": { file: "worlds/disney castle", data: true, secondary: "terra", },
+    "Space Paranoids": { file: "worlds/space paranoids", total: 2, },
+    "The World That Never Was": { file: "worlds/the world that never was", data: true, },
+    "Atlantica": { file: "worlds/atlantica", total: 5, data: true, },
+    "100 Acre Wood": { file: "worlds/100 acre wood", total: 6, },
 
-  drives: [ "Valor", "Wisdom", "Limit", "Master", "Final", ],
+    // Drives
+    "Valor Form": { file: "drive/valor", total: 7, secondary: "triangle", secondaryClass: "drive", },
+    "Wisdom Form": { file: "drive/wisdom", total: 7, secondary: "triangle", secondaryClass: "drive", },
+    "Limit Form": { file: "drive/limit", total: 7, secondary: "triangle", secondaryClass: "drive", },
+    "Master Form": { file: "drive/master", total: 7, secondary: "triangle", secondaryClass: "drive", },
+    "Final Form": { file: "drive/final", total: 7, secondary: "triangle", secondaryClass: "drive", },
 
-  other: {
+    // Magic
     "Fire": { file: "magic/fire", total: 3, },
     "Blizzard": { file: "magic/blizzard", total: 3, },
     "Thunder": { file: "magic/thunder", total: 3, },
     "Cure": { file: "magic/cure", total: 3, },
     "Reflect": { file: "magic/reflect", total: 3, },
     "Magnet": { file: "magic/magnet", total: 3, },
+
+    // Summons
     "Baseball Charm (Chicken Little)": { file: "charms/baseball", },
     "Lamp Charm (Genie)": { file: "charms/lamp", },
     "Ukelele Charm (Stitch)": { file: "charms/ukelele", },
     "Feather Charm (Peter Pan)": { file: "charms/feather", },
+
+    // Cup
     "Cup Tournaments": { file: "cup", total: 4, },
+
+    // Proofs/promise charm
     "Promise Charm": { file: "proofs_promise/promise", },
     "Proof of Nonexistence": { file: "proofs_promise/non-existent", },
     "Proof of Connection": { file: "proofs_promise/connections", },
     "Proof of Tranquility": { file: "proofs_promise/tranquil", },
-  }
+  },
 }
