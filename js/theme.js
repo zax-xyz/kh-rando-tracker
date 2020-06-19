8f5c2e83a0f2258e4eef9d6da5371c062e751f4e
Michael Vo <zax@zaxu.xyz>
2020-06-19 12:43:32 +1000

    use localStorage instead of cookies

--- js/theme.js
+++ js/theme.js
@@ -1,17 +1,12 @@
 // Theme script as separate file loaded first to avoid white flash
 function setTheme() {
   document.documentElement.setAttribute("theme", theme);
-  const cookieAge = 60 * 60 * 24 * 365;  // One year
-  document.cookie = `theme=${theme}; max-age=${cookieAge}`;
+  localStorage.theme = theme;
 }
 
-var theme = document.cookie
-  .split("; ")
-  .find((row) => row.startsWith("theme"));
+var theme = localStorage.theme;
 
-if (theme) {
-  theme = theme.split("=")[1];
-} else {
+if (theme === undefined) {
   theme = "dark";
 }
 
