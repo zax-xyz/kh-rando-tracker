b6e5902d1040ee1b850bcdece3a9402ea72dc220
Michael Vo <zax@zaxu.xyz>
2020-07-18 21:53:05 +1000

    add columns option

--- js/footer.js
+++ js/footer.js
@@ -23,7 +23,6 @@ document.onkeydown = (event) => {
 };
 
 const themeElem = document.getElementById("theme");
-
 themeElem.checked = theme === "dark";
 themeElem.onchange = (event) => {
   theme = event.target.checked ? "dark" : "light";
@@ -31,8 +30,18 @@ themeElem.onchange = (event) => {
 };
 
 const scrollElem = document.getElementById("scroll");
-
 scrollElem.checked = localStorage.scroll === "true";
 scrollElem.onchange = (event) => {
   localStorage.scroll = event.target.checked;
 };
+
+const columnsElem = document.getElementById("columns");
+columnsElem.value = localStorage.columns ?? null;
+columnsElem.oninput = (event) => {
+  const columns = event.target.value;
+  const grid = document.querySelector(".grid");
+
+  grid.style.gridTemplateColumns = columns ? `repeat(${columns}, auto)`: null;
+  localStorage.columns = columns;
+};
+columnsElem.oninput({ target: columnsElem });
