abb306fbab312acbeb1e317d6cbba3b7f8c517f2
zaxutic <zaxutic@gmail.com>
2020-07-18 07:42:14 +0000

    Deploying to gh-pages from  @ 4dd42f0bb518199310516c432c7615f0d541855d 🚀

--- js/theme.js
+++ js/theme.js
@@ -1,1 +1,1 @@
-function setTheme(){document.documentElement.setAttribute("theme",theme),localStorage.theme=theme}var theme=localStorage.theme;void 0===theme&&(theme="dark"),"dark"===theme?setTheme():theme="light";
+function setTheme(){document.documentElement.setAttribute("theme",theme),localStorage.theme=theme}var theme=localStorage.theme;theme===void 0&&(theme="dark"),"dark"===theme?setTheme():theme="light";
