57bbe923631330d4ffc07bb72c0cd8036fe26df4
Michael Vo <zax@zaxu.xyz>
2020-08-12 15:36:03 +1000

    make /televo redirect to base

--- televo/js/theme.js
+++
@@ -1,3 +0,0 @@
-"use strict";function setTheme(){document.documentElement.setAttribute("theme",theme);try{localStorage.theme=theme}catch(a){}}try{var theme=localStorage.theme}catch(a){console.error("Could not read localStorage, using defaults")}theme===void 0&&(theme="dark"),"dark"===theme?setTheme():theme="light";
-
-//# sourceMappingURL=theme.js.map
