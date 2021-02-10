9e13fa885716870757ca7dd94fa63124a32ddf08
Michael Vo <zax@zaxu.xyz>
2021-02-10 23:44:10 +1100

    deprecated

--- frontend/js/footer.js
+++ frontend/js/footer.js
@@ -1,21 +1,22 @@
 // Open relevant popup if its button is clicked
-$$("footer .popup > button").forEach(elem => {
-  elem.onclick = event => {
+$$("footer .popup > button").forEach((elem) => {
+  elem.onclick = (event) => {
     elem.nextElementSibling.classList.toggle("active");
-  }
+  };
 });
 
+$("#legacy").click();
+
 // Hide popup when clicking outside its area
-$$("footer .popup > .content").forEach(elem => {
-  elem.onmousedown = event => {
+$$("footer .popup > .content").forEach((elem) => {
+  elem.onmousedown = (event) => {
     // Remove active if target wasn't a child
-    if (elem === event.target)
-      elem.classList.remove("active");
-  }
+    if (elem === event.target) elem.classList.remove("active");
+  };
 });
 
 // Hide popups on Escape key
-document.onkeydown = event => {
+document.onkeydown = (event) => {
   if (event.key === "Escape") {
     $("footer .popup > .content.active")?.classList.remove("active");
   }
@@ -23,80 +24,90 @@ document.onkeydown = event => {
 
 const themeElem = $("#theme");
 themeElem.checked = theme === "dark";
-themeElem.onchange = event => {
+themeElem.onchange = (event) => {
   theme = event.target.checked ? "dark" : "light";
   setTheme();
 };
 
 const scrollElem = $("#scroll");
-try { scrollElem.checked = localStorage.scroll === "true" }
-catch {}
-scrollElem.onchange = event => {
-  try { localStorage.scroll = event.target.checked }
-  catch {}
+try {
+  scrollElem.checked = localStorage.scroll === "true";
+} catch {}
+scrollElem.onchange = (event) => {
+  try {
+    localStorage.scroll = event.target.checked;
+  } catch {}
 };
 
 const columnsElem = $("#columns");
-try { columnsElem.value = localStorage.columns ?? null }
-catch {}
-columnsElem.oninput = event => {
+try {
+  columnsElem.value = localStorage.columns ?? null;
+} catch {}
+columnsElem.oninput = (event) => {
   const columns = event.target.value;
   const grid = $(".grid");
 
-  grid.style.gridTemplateColumns = columns ? `repeat(${columns}, auto)`: null;
+  grid.style.gridTemplateColumns = columns ? `repeat(${columns}, auto)` : null;
 
-  try { localStorage.columns = columns }
-  catch {}
+  try {
+    localStorage.columns = columns;
+  } catch {}
 };
 columnsElem.oninput({ target: columnsElem });
 
 const bgInputElem = $("#background");
-try { bgInputElem.value = localStorage.bg ?? null }
-catch {}
-bgInputElem.oninput = event => {
+try {
+  bgInputElem.value = localStorage.bg ?? null;
+} catch {}
+bgInputElem.oninput = (event) => {
   const bg = event.target.value;
 
-  [ $(".grid"), $("footer") ].forEach(elem => {
-    elem.style.background = bg;
-  });
-  
-  $$("footer .popup .content .body").forEach(elem => {
+  [$(".grid"), $("footer")].forEach((elem) => {
     elem.style.background = bg;
   });
 
-  try { localStorage.bg = bg }
-  catch {}
+  $$("footer .popup .content .body").forEach((elem) => {
+    elem.style.background = bg;
+  });
+
+  try {
+    localStorage.bg = bg;
+  } catch {}
 };
 bgInputElem.oninput({ target: bgInputElem });
 
 const atlantica100Acre = $("#atlantica_100_acre");
-try { atlantica100Acre.checked = localStorage.atlantica100Acre === "true" }
-catch {}
-atlantica100Acre.onchange = event => {
+try {
+  atlantica100Acre.checked = localStorage.atlantica100Acre === "true";
+} catch {}
+atlantica100Acre.onchange = (event) => {
   // Lower total if setting enabled
   const value = event.target.checked ? 5 : 6;
 
-  [ $(".atlantica"), $(".hundred_acre") ].forEach(elem => {
+  [$(".atlantica"), $(".hundred_acre")].forEach((elem) => {
     elem.dataset.total = value;
   });
 
-  try { localStorage.atlantica100Acre = event.target.checked }
-  catch {}
+  try {
+    localStorage.atlantica100Acre = event.target.checked;
+  } catch {}
 };
 atlantica100Acre.onchange({ target: atlantica100Acre });
 
 const disableShadows = $("#disable_shadows");
-try { disableShadows.checked = localStorage.disableShadows === "true" }
-catch {}
-disableShadows.onchange = event => {
+try {
+  disableShadows.checked = localStorage.disableShadows === "true";
+} catch {}
+disableShadows.onchange = (event) => {
   const filter = event.target.checked ? "none" : null;
 
-  $$("img").forEach(elem => {
+  $$("img").forEach((elem) => {
     elem.style.filter = filter;
   });
 
-  try { localStorage.disableShadows = event.target.checked }
-  catch {}
+  try {
+    localStorage.disableShadows = event.target.checked;
+  } catch {}
 };
 disableShadows.onchange({ target: disableShadows });
 
@@ -104,47 +115,52 @@ const icons = Array.from($$(".item"));
 const grid = $(".grid");
 
 const iconOrder = $("#order");
-try { iconOrder.value = localStorage.order ?? null }
-catch {}
-iconOrder.oninput = event => {
+try {
+  iconOrder.value = localStorage.order ?? null;
+} catch {}
+iconOrder.oninput = (event) => {
   const order = event.target.value.trim();
   grid.innerHTML = "";
 
-  if (order)
-    order.split(" ").forEach(i => grid.appendChild(icons[i - 1]));
-  else
-    icons.forEach(icon => grid.appendChild(icon));
+  if (order) order.split(" ").forEach((i) => grid.appendChild(icons[i - 1]));
+  else icons.forEach((icon) => grid.appendChild(icon));
 
-  try { localStorage.order = event.target.value }
-  catch {}
+  try {
+    localStorage.order = event.target.value;
+  } catch {}
 };
 if (iconOrder.value) iconOrder.oninput({ target: iconOrder });
 
 const iconRemove = $("#remove");
-try { iconRemove.value = localStorage.remove ?? null }
-catch {}
-iconRemove.oninput = event => {
-  const remove = event.target.value.trim().split(" ").map(i => parseInt(i) - 1);
+try {
+  iconRemove.value = localStorage.remove ?? null;
+} catch {}
+iconRemove.oninput = (event) => {
+  const remove = event.target.value
+    .trim()
+    .split(" ")
+    .map((i) => parseInt(i) - 1);
   grid.innerHTML = "";
 
   for (const [index, icon] of icons.entries()) {
-    if (!(remove.includes(index))) {
+    if (!remove.includes(index)) {
       grid.appendChild(icon);
     }
   }
 
-  try { localStorage.remove = event.target.value }
-  catch {}
+  try {
+    localStorage.remove = event.target.value;
+  } catch {}
 };
 if (iconRemove.value) iconRemove.oninput({ target: iconRemove });
 
 function useStyle(style) {
   const dir = style ? `img/${style}/` : "img/";
-  $$(".icon").forEach(elem => {
+  $$(".icon").forEach((elem) => {
     const src = elem.dataset.src;
     let iconDir = dir;
     if (style == "classic" && !(src.startsWith("drive/") || src.startsWith("summons/"))) {
-      iconDir = "img/"
+      iconDir = "img/";
     }
     elem.src = iconDir + src;
   });
@@ -161,8 +177,9 @@ try {
 } catch {
   useStyle("");
 }
-icon_style.onchange = ev => {
+icon_style.onchange = (ev) => {
   useStyle(ev.target.value);
-  try { localStorage.iconStyle = ev.target.value }
-  catch {}
+  try {
+    localStorage.iconStyle = ev.target.value;
+  } catch {}
 };
