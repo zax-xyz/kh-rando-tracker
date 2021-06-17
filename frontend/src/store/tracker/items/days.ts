86efdaee773f452aad36613968db0292a8606537
Michael Vo <zax@zaxu.xyz>
2021-06-17 17:24:08 +1000

    Fix days OC icon

--- frontend/src/store/tracker/items/days.ts
+++ frontend/src/store/tracker/items/days.ts
@@ -5,7 +5,7 @@ export const items: Items = {
   "worlds/twilight_town": item({ category: "worlds" }),
   "worlds/agrabah": item({ category: "worlds" }),
   "worlds/beast's_castle": item({ category: "worlds" }),
-  "worlds/olympus_coliseum": item({ category: "worlds" }),
+  "worlds/kh1/olympus_coliseum": item({ category: "worlds" }),
   "worlds/halloween_town": item({ category: "worlds" }),
   "worlds/kh1/wonderland": item({ category: "worlds", categoryExclude: IconStyle.CLASSIC }),
   "worlds/kh1/neverland": item({ category: "worlds", categoryExclude: IconStyle.CLASSIC }),
