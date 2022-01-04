50ca73917f8075e3fda59a6889296e1927cea7c8
Michael Vo <zax@zaxu.xyz>
2022-01-04 22:57:23 +1100

    fix(settings): import/export State as type

--- src/store/settings/index.ts
+++ src/store/settings/index.ts
@@ -2,7 +2,8 @@ import mutations from "./mutations";
 import actions from "./actions";
 import { state } from "./state";
 
-export { IconStyle, Game, State } from "./state";
+export { IconStyle, Game } from "./state";
+export type { State } from "./state";
 
 export const settings = {
   namespaced: true,
